415165c7d373351ad86b37c7a32a0dadd0bc293f
chemicalRatt <chemicalRatt@outlook.com>
2023-12-23 12:40:11 -0500

    refactor object as factory function

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const libraryList = document.getElementById("library-list");
-
+/*
 function Book(title, author){
     this.title = title;
     this.author = author;
 }
 
+refactored as factory function:
+*/
+function createBook(title, author){
+    return {title, author};
+}
+
 function addBook(){
     const bTitle = document.getElementById("book-title");
     const bAuthor = document.getElementById("book-author");
-    const book = new Book(bTitle.value,bAuthor.value);
+    //const book = new Book(bTitle.value,bAuthor.value);
+    const book = createBook(bTitle.value,bAuthor.value);
 
     let tmp = document.createElement("li");
     tmp.innerText = book.title + " by " + book.author + " ";
